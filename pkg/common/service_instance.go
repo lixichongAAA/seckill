703f42ed4c553357eac47deb307b27bf87233f03
Lxc <1104426357@qq.com>
2023-03-30 17:39:26 +0800

    共同依赖, 一些基础结构

--- pkg/common/service_instance.go
+++ pkg/common/service_instance.go
@@ -1,10 +1,11 @@
 package common
 
+// 表示一个服务实例
 type ServiceInstance struct {
-	Host      string // Host
+	Host      string // 主机ip Host
 	Port      int    // Post
-	Weight    int    // 权重
-	CurWeight int    // 当前权重
+	Weight    int    // 权重, 用于负载均衡， 表示配置的服务实例权重， 固定不变
+	CurWeight int    // 当前权重, 用于负载均衡，服务实例目前的权重，一开始为零，之后会动态调整
 
-	GrpcPort int
+	GrpcPort int // RPC 服务的端口号
 }
